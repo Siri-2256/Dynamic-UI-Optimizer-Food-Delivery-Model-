f4c5444f3821b278c1f9d6fb49968710645f463b
Siri-2256 <2303a52256@sru.edu.in>
2025-12-16 12:36:48 +0530

    Add routing for Home and Analytics pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Analytics from "./pages/Analytics";
+
+export default function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/analytics" element={<Analytics />} />
+      </Routes>
+    </div>
+  );
+}
 
