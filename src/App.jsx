f1d29b15b0590723dd9b2d77daf91d2063a7ea9b
Siri-2256 <2303a52256@sru.edu.in>
2025-12-16 12:43:19 +0530

    Wrap Routes in AppProvider for context access

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Analytics from "./pages/Analytics";
+import { AppProvider } from "./context/AppContext";
 
 export default function App() {
   return (
-    <div>
+    <AppProvider>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/analytics" element={<Analytics />} />
       </Routes>
-    </div>
+    </AppProvider>
   );
 }
-
