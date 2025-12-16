b031193cdf03ae188c4fec34060a6c899280cd66
Siri-2256 <2303a52256@sru.edu.in>
2025-12-16 14:05:26 +0530

    Add Navbar component to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Analytics from "./pages/Analytics";
+import Navbar from "./components/Navbar";
 import { AppProvider } from "./context/AppContext";
 
 export default function App() {
   return (
     <AppProvider>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/analytics" element={<Analytics />} />
