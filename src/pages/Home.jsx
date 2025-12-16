4baca52ea8a8ec8a94fc77b0d25e8271fdc24e3e
Siri-2256 <2303a52256@sru.edu.in>
2025-12-16 12:41:19 +0530

    Add button to track clicks in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,17 @@
+import { useApp } from "../context/AppContext";
+
+export default function Home() {
+  const { trackClick } = useApp();
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>🍔 Food Delivery App</h1>
+      <p>Order food from your favorite restaurants</p>
+
+      <button onClick={trackClick}>
+        Explore Restaurants
+      </button>
+    </div>
+  );
+}
 
